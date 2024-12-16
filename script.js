4533eb492233ccaddc52380fe6aec418230be363
Gigi Nakhutsrishvili <nakhutsrishviligigi@gmail.com>
2024-12-16 13:32:47 +0400

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,37 @@ const lastNameError = document.getElementById('lastname-error');
 const email = document.getElementById('email');
 const emailError = document.getElementById('email-error');
 const password = document.getElementById('password');
-const passwordError = document.getElementById('password-error')
+const passwordError = document.getElementById('password-error');
+
+document.getElementById('Submit').addEventListener('submit',function(event) {
+    event.preventDefault();
+
+const namePattern = 
+const emailPattern =
+const passwordPattern =
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
